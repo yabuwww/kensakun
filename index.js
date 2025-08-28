eaa071c20f0dfd7f28f56d67183cc3289c113e6f
yabuwww <yabuhara.ayumu@gmail.com>
2025-08-28 17:19:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ let recognition = null;
 
 
 // --- API Initialization ---
-const API_KEY = AIzaSyAR8SahNPyptp0mQqHwdnqjtf49cmkIx0s;
+const API_KEY = 'AIzaSyAR8SahNPyptp0mQqHwdnqjtf49cmkIx0s';
 if (!API_KEY) {
   showError('APIキーが設定されていません。');
   throw new Error('API_KEY is not set');
