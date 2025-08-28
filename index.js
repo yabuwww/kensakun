de8180a2fa3718f94cf96a67c812deda015474e5
yabuwww <yabuhara.ayumu@gmail.com>
2025-08-28 16:30:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ let recognition = null;
 
 
 // --- API Initialization ---
-const API_KEY = process.env.API_KEY;
+const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 if (!API_KEY) {
   showError('APIキーが設定されていません。');
   throw new Error('API_KEY is not set');
