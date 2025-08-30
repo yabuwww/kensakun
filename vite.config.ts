6052f34b681d494444e4148f32f16335a882ad7b
yabuwww <yabuhara.ayumu@gmail.com>
2025-08-30 13:35:24 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  // publicDirを'public'に指定
+  publicDir: 'public', 
+  build: {
+    outDir: 'dist',
+  },
+})
