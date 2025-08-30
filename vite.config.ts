3f7da552e527c2c590cad42e7aeb804fa590df76
yabuwww <yabuhara.ayumu@gmail.com>
2025-08-30 13:47:57 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // publicDirを'public'に指定
-  publicDir: 'public', 
+  plugins: [react()],
   build: {
     outDir: 'dist',
-  },
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
